1d5a42ac7db968c696c6f406369a0d8f70e15ec3
Yoshihide Jimbo <yjimbo@gmail.com>
2017-12-03 15:14:10 +0900

    Separete webpack configurations for each environment

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const lazyResources = require('./src/lazy-resources.json');
+
+const entry = {};
+lazyResources.forEach(resource => {
+  entry[resource] = `./src/${resource}`;
+});
+
+module.exports = {
+  entry: {
+    'iota-app': './src/iota-app.js',
+    ...entry,
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin(['images', 'manifest.json', 'service-worker.js']),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
