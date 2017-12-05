3425228de63295c2b0f0938087b978f3d47c357e
Yoshihide Jimbo <yjimbo@gmail.com>
2017-12-05 10:48:13 +0900

    Add footer

--- src/iota-app-footer.js
+++ src/iota-app-footer.js
@@ -23,6 +23,8 @@ class IotaAppFooter extends PolymerElement {
 
       .donation {
         padding: 8px 4px;
+        font-size: 14px;
+        color: var(--app-primary-color);
       }
 
       pre {
@@ -31,6 +33,10 @@ class IotaAppFooter extends PolymerElement {
         word-wrap: break-word;
         overflow: auto;
         font-size: 13px;
+        border: 1px solid var(--app-border-color);
+        background-color: #fff;
+        border-radius: 4px;
+        padding: 4px 8px;
       }
 
       address {
@@ -38,18 +44,18 @@ class IotaAppFooter extends PolymerElement {
         font-style: normal;        
       }
       </style>
-      <div class="disclaimer">
-        <ul>
-          <li>本ウェブサイトで表示しているIOTA/JPYの価格は、<a href="https://www.cryptocompare.com/">cryptocompare.com</a> が提供するマーケットデータから算出した概算値です。実際の取引価格とは異なります。</li>
-          <li>当ウェブサイトは、当ウェブサイトに含まれる情報もしくは内容をご利用されたことで直接・間接的に生じた損失に関し、一切責任を負わないものとします。</li>
-        </ul>
-      </div>
       <div class="donation">
         <p>Feel like giving me ☕️ or 🍺 ?</p>
         IOTA Address:
         <pre>ZMSIPJODCKDCVLAKMF9ECFVREWKRPRHVUKP9GBKMCWGEYFCOBTIJWRLVNPHPCYUJKGQRGZANUJVXXALVXCJD9SXYC9</pre>
       </div>
-      <address>&copy; ${new Date().getFullYear()} Yoshihide Jimbo. All rights reserved.</address>
+      <div class="disclaimer">
+      <ul>
+        <li>本ウェブサイトで表示しているIOTA/JPYの価格は、<a href="https://www.cryptocompare.com/">cryptocompare.com</a> が提供するマーケットデータから算出した概算値です。実際の取引価格とは異なります。</li>
+        <li>当ウェブサイトは、当ウェブサイトに含まれる情報もしくは内容をご利用されたことで直接・間接的に生じた損失に関し、一切責任を負わないものとします。</li>
+      </ul>
+    </div>
+    <address>&copy; ${new Date().getFullYear()} Yoshihide Jimbo. All rights reserved.</address>
       `;
   }
 }
