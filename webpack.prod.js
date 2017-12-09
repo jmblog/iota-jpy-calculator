a80fb278d27272d525440273457fc7f3bf8b099e
Yoshihide Jimbo <yjimbo@gmail.com>
2017-12-09 15:42:39 +0900

    Stop compressing manually because firebase hosting does it automatically

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const ZopfliPlugin = require('zopfli-webpack-plugin');
 
 module.exports = merge(common, {
-  plugins: [
-    new UglifyJsPlugin(),
-    new ZopfliPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'zopfli',
-      test: /\.(js|html)$/,
-      threshold: 10240,
-      minRatio: 0.8,
-    }),
-  ],
+  plugins: [new UglifyJsPlugin()],
 });
